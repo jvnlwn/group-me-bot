10761dc25ea83a5ab251ba619dc8e2ff4291996e
Joe Van Leeuwen <jvnlwn@gmail.com>
2025-05-19 07:40:15 -0400

    update: properly set poll expiration time

--- actions/poll/action.ts
+++ actions/poll/action.ts
@@ -1,10 +1,11 @@
+import { DateTime } from "luxon"
 import { getActivePoll, getPolls } from "../../lib/poll"
 import { ActionFn } from "../../types"
 
 // Util for creating a poll in a groupme group.
 // The poll is hardcoded to ask if people are coming tomorrow for soccer,
 // so, very specific use case.
-const action: ActionFn = async ({ group_id: groupId }) => {
+const action: ActionFn = async ({ group_id: groupId, created_at }) => {
   const token = process.env.GROUP_ME_API_ACCESS_TOKEN
 
   const activePoll = await getActivePoll({ polls: await getPolls({ groupId }) })
@@ -12,6 +13,26 @@ const action: ActionFn = async ({ group_id: groupId }) => {
   if (activePoll) {
     throw new Error("An active poll already exists.")
   }
+
+  // Set the expiration relative to the created date.
+  const epxirationUTCDate = new Date(created_at * 1000)
+
+  // Create a date expiring at 9pm ET. You may have to define timezone for
+  // each group if you don't want to hardcode this.
+  const epxirationETDateTime = DateTime.fromObject(
+    {
+      year: epxirationUTCDate.getFullYear(),
+      month: epxirationUTCDate.getMonth() + 1,
+      day: epxirationUTCDate.getDate(),
+      hour: 21,
+      minute: 0
+    },
+    { zone: "America/New_York" }
+  )
+
+  // Now convert to UTC JS Date.
+  const expirationUTCTime =
+    epxirationETDateTime.toUTC().toJSDate().getTime() / 1000
 
   const response = await fetch(
     `https://api.groupme.com/v3/poll/${groupId}?token=${token}`,
@@ -23,14 +44,7 @@ const action: ActionFn = async ({ group_id: groupId }) => {
       body: JSON.stringify({
         subject: "⚽",
         options: [{ title: "Yes" }, { title: "No" }],
-        expiration: Math.floor(
-          // 12 hours from now
-          // new Date(Date.now() + 1000 * 60 * 60 * 12).getTime() / 1000
-          // 9 PM EST, current day (doesn't take into account DST).
-          new Date(
-            new Date().setHours(21, 0, 0, 0) + 1000 * 60 * 60 * 12
-          ).getTime() / 1000
-        ),
+        expiration: expirationUTCTime,
         visibility: "public",
         type: "single"
       })
